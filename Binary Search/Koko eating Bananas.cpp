18e8b9f787db640ec9f44fbb828e87ef615808a5
vignesh-nair07 <vigvishnair@gmail.com>
2026-09-09 09:10:05 +0530

    Add solution for Koko Eating Bananas problem
    Implement Koko Eating Bananas problem solution using binary search to find the minimum eating speed.

---
+++ Binary Search/Koko eating Bananas.cpp
@@ -0,0 +1,64 @@
+/*
+875. Koko Eating Bananas
+
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+
+ 
+
+Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+Example 2:
+
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+ 
+
+Constraints:
+
+1 <= piles.length <= 104
+piles.length <= h <= 109
+1 <= piles[i] <= 109
+*/
+
+//SOLUTION
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& a, int h) {
+        using ll=long long;
+        
+        int l=1;
+        int r=*max_element(a.begin(),a.end());
+        int ans=1;
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            ll sum=0;
+
+            for(int i=0;i<a.size();i++){
+                sum+=(a[i]+mid-1)/mid;
+            }
+            if(sum<=h){
+                ans=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
+//TC: O(nlog(max piles))
+//SC: O(1)
